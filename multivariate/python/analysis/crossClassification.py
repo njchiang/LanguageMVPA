4a82a72ff6847f1ddad77e32a3d58d351a15cd0c
njchiang <jeff.njchiang@gmail.com>
2015-11-12 21:36:46 -0800

    delted comment

--- multivariate/python/analysis/crossClassification.py
+++ multivariate/python/analysis/crossClassification.py
@@ -2,7 +2,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 # using the chunks to do cross-classification.
-# blahblahblahblah
 from mvpa2.suite import *
 import os
 projectDir="Z:\\fmri\\LanguageMVPA"
