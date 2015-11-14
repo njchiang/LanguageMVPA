a0b651634f5c79a9b8e82eebbce0475dbdb1b88c
njchiang <jeff.njchiang@gmail.com>
2015-11-14 12:55:02 -0800

    added comment

--- multivariate/python/analysis/roiAnalysis.py
+++ multivariate/python/analysis/roiAnalysis.py
@@ -2,7 +2,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 # configure for windows
-
+# test test test
 from mvpa2.suite import *
 import os
 
