5c76836ea7d75fc94d7144df22079b9c304078bc
njchiang <jeff.njchiang@gmail.com>
2015-11-19 10:46:21 -0800

    edit postprocess
    modify post process to account for concatenation

---
+++ multivariate/bash/postprocess_pyMVPA.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+:<<doc
+registers stat maps to standard space
+runs randomise
+doc
+#standard=${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz
+projectDir=/Volumes/fmri/LanguageMVPA
+#projectDir=/media/sf_fmri/LanguageMVPA
+refImage=${projectDir}/fnirt/MNI152_T1_3mm_brain.nii.gz
+refMask=${projectDir}/fnirt/MNI152_T1_3mm_brain_mask.nii.gz
+targetDir=${projectDir}/Maps/PyMVPA
+
+cd ${targetDir}
+#move raw outputs
+mkdir raw
+mkdir std
+mv *.nii.gz raw
+cd raw
+for model in syntax_lang syntax_pic #ActPass_lang ActPass_pic RelCan_lang RelCan_pic #cross_verb_L2P cross_verb_P2L
+do
+	for indMap in `ls | grep _${model}_concat_cvsl.nii.gz`
+	do
+		sub=`echo ${indMap} | cut -d '_' -f1`
+		meanMap=${sub}_${model}
+		fslmaths ${indMap} -Tmean ${meanMap}
+		echo ${sub}
+		fslmaths ${projectDir}/masks/sub/${sub}_grayMatter.nii.gz -bin tmp.nii.gz
+		fslmaths tmp.nii.gz -mul 25 tmp.nii.gz
+		fslmaths ${meanMap} -mul 100 -sub tmp.nii.gz rnd_${meanMap}
+		regMat=${projectDir}/registration/${sub}_example_func2standard.mat
+
+		flirt -in rnd_${meanMap} -out ../std/std_${meanMap} -ref ${refImage} \
+			-applyxfm -init ${regMat}
+		rm rnd_${meanMap}
+	done
+fslmerge -t ../Group_${model}.nii.gz ../std/std*_${model}*nii.gz
+randomise -i ../Group_${model}.nii.gz -o ../n1000_${model} -1 -T --uncorrp -n 1000 -m ${projectDir}/masks/3mm_grayMatter.hdr
+done
+
