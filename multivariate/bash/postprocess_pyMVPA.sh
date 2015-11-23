701f264cfc474d654641a9ff434e8522469a7277
njchiang <jeff.njchiang@gmail.com>
2015-11-23 10:30:59 -0800

    update postprocess

--- multivariate/bash/postprocess_pyMVPA.sh
+++ multivariate/bash/postprocess_pyMVPA.sh
@@ -16,9 +16,11 @@ mkdir raw
 mkdir std
 mv *.nii.gz raw
 cd raw
-for model in syntax_lang syntax_pic #ActPass_lang ActPass_pic RelCan_lang RelCan_pic #cross_verb_L2P cross_verb_P2L
+:<<com
+for model in syntax_lang syntax_pic anim_verb_lang anim_verb_pic \
+	inanim_verb_lang inanim_verb_pic 
 do
-	for indMap in `ls | grep _${model}_concat_cvsl.nii.gz`
+	for indMap in `ls | grep grayMatter_${model}_concat_cvsl.nii.gz`
 	do
 		sub=`echo ${indMap} | cut -d '_' -f1`
 		meanMap=${sub}_${model}
@@ -31,7 +33,51 @@ do
 
 		flirt -in rnd_${meanMap} -out ../std/std_${meanMap} -ref ${refImage} \
 			-applyxfm -init ${regMat}
-		rm rnd_${meanMap}
+		rm rnd_${meanMap}.nii.gz
+	done
+fslmerge -t ../Group_${model}.nii.gz ../std/std*_${model}*nii.gz
+randomise -i ../Group_${model}.nii.gz -o ../n1000_${model} -1 -T --uncorrp -n 1000 -m ${projectDir}/masks/3mm_grayMatter.hdr
+done
+
+for model in ActPass_lang ActPass_pic RelCan_lang RelCan_pic anim_ActPass_lang \
+	anim_ActPass_pic inanim_ActPass_lang inanim_ActPass_pic anim_RelCan_lang \
+	anim_RelCan_pic inanim_RelCan_lang inanim_RelCan_pic syntax_pic
+do
+	for indMap in `ls | grep grayMatter_${model}_concat_cvsl.nii.gz`
+	do
+		sub=`echo ${indMap} | cut -d '_' -f1`
+		meanMap=${sub}_${model}
+		fslmaths ${indMap} -Tmean ${meanMap}
+		echo ${sub}
+		fslmaths ${projectDir}/masks/sub/${sub}_grayMatter.nii.gz -bin tmp.nii.gz
+		fslmaths tmp.nii.gz -mul 50 tmp.nii.gz
+		fslmaths ${meanMap} -mul 100 -sub tmp.nii.gz rnd_${meanMap}
+		regMat=${projectDir}/registration/${sub}_example_func2standard.mat
+
+		flirt -in rnd_${meanMap} -out ../std/std_${meanMap} -ref ${refImage} \
+			-applyxfm -init ${regMat}
+		rm rnd_${meanMap}.nii.gz
+	done
+fslmerge -t ../Group_${model}.nii.gz ../std/std*_${model}*nii.gz
+randomise -i ../Group_${model}.nii.gz -o ../n1000_${model} -1 -T --uncorrp -n 1000 -m ${projectDir}/masks/3mm_grayMatter.hdr
+done
+com
+for model in verb_lang verb_pic
+do
+	for indMap in `ls | grep grayMatter_${model}_concat_cvsl.nii.gz`
+	do
+		sub=`echo ${indMap} | cut -d '_' -f1`
+		meanMap=${sub}_${model}
+		fslmaths ${indMap} -Tmean ${meanMap}
+		echo ${sub}
+		fslmaths ${projectDir}/masks/sub/${sub}_grayMatter.nii.gz -bin tmp.nii.gz
+		fslmaths tmp.nii.gz -mul 12.5 tmp.nii.gz
+		fslmaths ${meanMap} -mul 100 -sub tmp.nii.gz rnd_${meanMap}
+		regMat=${projectDir}/registration/${sub}_example_func2standard.mat
+
+		flirt -in rnd_${meanMap} -out ../std/std_${meanMap} -ref ${refImage} \
+			-applyxfm -init ${regMat}
+		rm rnd_${meanMap}.nii.gz
 	done
 fslmerge -t ../Group_${model}.nii.gz ../std/std*_${model}*nii.gz
 randomise -i ../Group_${model}.nii.gz -o ../n1000_${model} -1 -T --uncorrp -n 1000 -m ${projectDir}/masks/3mm_grayMatter.hdr
