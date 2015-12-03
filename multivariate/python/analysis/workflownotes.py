18f723124dcca7f4fb80e904832d53283a7c3fae
njchiang <jeff.njchiang@gmail.com>
2015-12-03 09:16:42 -0800

    update searchlight scripts... there was an error before where it was only saving the first iteration

--- multivariate/python/analysis/workflownotes.py
+++ multivariate/python/analysis/workflownotes.py
@@ -127,3 +127,52 @@ thread1.start()
 thread2.start()
 
 print "Exiting Main Thread"
+
+
+# MULTISUBJECT
+
+from mvpa2.suite import *
+
+verbose.level = 2
+
+verbose(1, "Loading data...")
+filepath = os.path.join(cfg.get('location', 'tutorial data'),
+                        'hyperalignment_tutorial_data.hdf5.gz')
+ds_all = h5load(filepath)
+# zscore all datasets individually
+_ = [zscore(ds) for ds in ds_all]
+# inject the subject ID into all datasets
+for i,sd in enumerate(ds_all):
+    sd.sa['subject'] = np.repeat(i, len(sd))
+# number of subjects
+nsubjs = len(ds_all)
+# number of categories
+ncats = len(ds_all[0].UT)
+# number of run
+nruns = len(ds_all[0].UC)
+verbose(2, "%d subjects" % len(ds_all))
+verbose(2, "Per-subject dataset: %i samples with %i features" % ds_all[0].shape)
+verbose(2, "Stimulus categories: %s" % ', '.join(ds_all[0].UT))
+
+# use same classifier
+clf = LinearCSVMC()
+
+# feature selection helpers
+nf = 100
+fselector = FixedNElementTailSelector(nf, tail='upper',
+                                      mode='select',sort=False)
+sbfs = SensitivityBasedFeatureSelection(OneWayAnova(), fselector,
+                                        enable_ca=['sensitivities'])
+# create classifier with automatic feature selection
+fsclf = FeatureSelectionClassifier(clf, sbfs)
+
+verbose(1, "Performing classification analyses...")
+verbose(2, "within-subject...", cr=False, lf=False)
+wsc_start_time = time.time()
+cv = CrossValidation(fsclf,
+                     NFoldPartitioner(attr='chunks'),
+                     errorfx=mean_match_accuracy)
+# store results in a sequence
+wsc_results = [cv(sd) for sd in ds_all]
+wsc_results = vstack(wsc_results)
+verbose(2, " done in %.1f seconds" % (time.time() - wsc_start_time,))
