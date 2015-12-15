34a959f7189998a942b6a71a3e37e31303271fe5
njchiang <jeff.njchiang@gmail.com>
2015-12-15 13:52:48 -0800

    learning graph theory

--- multivariate/R/graphTheoryExample.R
+++ multivariate/R/graphTheoryExample.R
@@ -20,15 +20,14 @@ library(mnormt)
 # input: connectivity matrix. 
 
 cMatrix <- replicate(10, rnorm(10)) 
-cMatrix <- read.csv("~/Desktop/UCLA/scratchpad/LanguageMVPA/Workbook1.csv", header=F)
-cMatrix <- read.table("~/Desktop/UCLA/scratchpad/LanguageMVPA/Workbook1.csv", header=F)
+cMatrix <- 1-read.csv("~/Desktop/UCLA/scratchpad/LanguageMVPA/Workbook1.csv", header=F)
 subData <- network(cMatrix)
-latentSub <- ergmm(subData ~ euclidean(d=2, G=1), verbose = TRUE)
+latentSub <- ergmm(subData ~ euclidean(d=2), verbose = TRUE)
 # commands after getting model
 plot(latentSub)
 # get probabilities of group membership
 latentSub$mkl 
 # Z coordinates for each node:
-latentSub$Z
+latentSub$mkl$Z
 # tab to autocomplete
 
