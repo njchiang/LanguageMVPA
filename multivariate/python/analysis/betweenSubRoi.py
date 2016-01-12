fix searchlight, and make between sub analysis that takes average activation for condition.

multivariate/python/analysis/betweenSubRoi.py
@@ -5,76 +5,68 @@
 from mvpa2.suite import *
 import os
 print "Initializing..."
-verbose(2, "between-subject (anatomically aligned)...", cr=False, lf=False)
-ds_mni = vstack(ds_all)
-mni_start_time = time.time()
-cv = CrossValidation(fsclf,
-                     NFoldPartitioner(attr='subject'),
-                     errorfx=mean_match_accuracy)
-bsc_mni_results = cv(ds_mni)
-verbose(2, "done in %.1f seconds" % (time.time() - mni_start_time,))
+# initialize paths
+projectDir = "D:\\fmri\\LanguageMVPA"
+codeDir = "D:\GitHub\LanguageMVPA\multivariate\python"
+betaType = "crossSub"  # tstat cope orig
+betaPath = os.path.join(projectDir, "betas", betaType)
+maskPath = os.path.join(projectDir, "masks")
+labelPath = os.path.join(codeDir, "labels")
+outPath = os.path.join(projectDir, "Maps")
 
-verbose(2, "between-subject (hyperaligned)...", cr=False, lf=False)
-hyper_start_time = time.time()
-bsc_hyper_results = []
-# same cross-validation over subjects as before
-cv = CrossValidation(clf, NFoldPartitioner(attr='subject'),
-                     errorfx=mean_match_accuracy)
-
-# leave-one-run-out for hyperalignment training
-for test_run in range(nruns):
-    # split in training and testing set
-    ds_train = [sd[sd.sa.chunks != test_run,:] for sd in ds_all]
-    ds_test = [sd[sd.sa.chunks == test_run,:] for sd in ds_all]
-
-    # manual feature selection for every individual dataset in the list
-    anova = OneWayAnova()
-    fscores = [anova(sd) for sd in ds_train]
-    featsels = [StaticFeatureSelection(fselector(fscore)) for fscore in fscores]
-    ds_train_fs = [featsels[i].forward(sd) for i, sd in enumerate(ds_train)]
+# initialize subjects, masks, contrast
+contrasts = ["verb", "syntax", "anim"]
+maskList = ["left_IFG_operc", "left_IFG_triang", "left_STG_post", "left_MTG_post", "langNet", "grayMatter"]
+mask = maskList[5]
+con = contrasts[2]
 
 
-    # Perform hyperalignment on the training data with default parameters.
-    # Computing hyperalignment parameters is as simple as calling the
-    # hyperalignment object with a list of datasets. All datasets must have the
-    # same number of samples and time-locked responses are assumed.
-    # Hyperalignment returns a list of mappers corresponding to subjects in the
-    # same order as the list of datasets we passed in.
+def loadData(stim, c, m):
+    cv_attr = SampleAttributes(os.path.join(labelPath, ("crossSub_" + c + "_attribute_labels.txt")))
+    bSeriesName = str(stim + "_" + c + ".nii.gz")
+    maskName = str("2mm_" + m + ".nii.gz")
+    bSeries = os.path.join(betaPath, bSeriesName)
+    maskFile = os.path.join(maskPath, maskName)
+    ds = (fmri_dataset(samples=bSeries, targets=cv_attr.targets, chunks=cv_attr.chunks, mask=maskFile))
+    return ds
 
+lds = loadData("lang", con, mask)
+pds = loadData("pic", con, mask)
 
-    hyper = Hyperalignment()
-    hypmaps = hyper(ds_train_fs)
+def initCV():
+    # initialize classifier
+    # clf = LinearCSVMC()
+    clf = LinearNuSVMC()
+    # clf = RbfNuSVMC()
+    # feature selection helpers
+    nf = 100
+    fselector = FixedNElementTailSelector(nf, tail='upper',
+                                          mode='select',sort=False)
+    sbfs = SensitivityBasedFeatureSelection(OneWayAnova(), fselector,
+                                            enable_ca=['sensitivities'])
+    # create classifier with automatic feature selection
+    fsclf = FeatureSelectionClassifier(clf, sbfs)
+    cv = CrossValidation(fsclf,
+                         NFoldPartitioner(attr='chunks'),
+                         errorfx=mean_match_accuracy)
+    # cv = CrossValidation(fclf, NFoldPartitioner(), errorfx=lambda p, t: np.mean(p == t),  enable_ca=['stats'])
+    return cv
 
-    # Applying hyperalignment parameters is similar to applying any mapper in
-    # PyMVPA. We start by selecting the voxels that we used to derive the
-    # hyperalignment parameters. And then apply the hyperalignment parameters
-    # by running the test dataset through the forward() function of the mapper.
+lcv = initCV()
+bsc_l_results = lcv(lds)
+pcv = initCV()
+bsc_p_results = pcv(pds)
+print "Avg between-subject accuracy: " + str(np.mean(bsc_l_results)) + " +/- " + str(np.std(bsc_l_results) / np.sqrt(16))
+print "Avg between-subject accuracy: " + str(np.mean(bsc_p_results)) + " +/- " + str(np.std(bsc_p_results) / np.sqrt(16))
 
-    ds_test_fs = [featsels[i].forward(sd) for i, sd in enumerate(ds_test)]
-    ds_hyper = [ hypmaps[i].forward(sd) for i, sd in enumerate(ds_test_fs)]
-
-    # Now, we have a list of datasets with feature correspondence in a common
-    # space derived from the training data. Just as in the between-subject
-    # analyses of anatomically aligned data we can stack them all up and run the
-    # crossvalidation analysis.
-
-    ds_hyper = vstack(ds_hyper)
-    # zscore each subject individually after transformation for optimal
-    # performance
-    zscore(ds_hyper, chunks_attr='subject')
-    res_cv = cv(ds_hyper)
-    bsc_hyper_results.append(res_cv)
-
-bsc_hyper_results = hstack(bsc_hyper_results)
-verbose(2, "done in %.1f seconds" % (time.time() - hyper_start_time,))
-
-verbose(1, "Average classification accuracies:")
-verbose(2, "within-subject: %.2f +/-%.3f"
-        % (np.mean(wsc_results),
-           np.std(wsc_results) / np.sqrt(nsubjs - 1)))
-verbose(2, "between-subject (anatomically aligned): %.2f +/-%.3f"
-        % (np.mean(bsc_mni_results),
-           np.std(np.mean(bsc_mni_results, axis=1)) / np.sqrt(nsubjs - 1)))
-verbose(2, "between-subject (hyperaligned): %.2f +/-%.3f" \
-        % (np.mean(bsc_hyper_results),
-           np.std(np.mean(bsc_hyper_results, axis=1)) / np.sqrt(nsubjs - 1)))
+ccv = initCV()
+cc = str("cross_" + con)
+cv_attr = SampleAttributes(os.path.join(labelPath, ("crossSub_" + cc + "_attribute_labels.txt")))
+bSeriesName = str(con + ".nii.gz")
+maskName = str("2mm_" + mask + ".nii.gz")
+bSeries = os.path.join(betaPath, bSeriesName)
+maskFile = os.path.join(maskPath, maskName)
+fds = (fmri_dataset(samples=bSeries, targets=cv_attr.targets, chunks=cv_attr.chunks, mask=maskFile))
+bsc_c_results = ccv(fds)
+print "Avg between-subject accuracy: " + str(np.mean(bsc_c_results[0]))
+print "Avg between-subject accuracy: " + str(np.mean(bsc_c_results[1]))