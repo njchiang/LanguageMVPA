64d9f51eaa5636dcab9a01be05b9691e2913ab95
njchiang <jeff.njchiang@gmail.com>
2016-04-11 17:45:46 -0700

    fixed prep roc

--- multivariate/python/utils/lmvpautils.py
+++ multivariate/python/utils/lmvpautils.py
@@ -1,17 +1,28 @@
 # utility functions used throughout lmvpa project
-
-
-def initpaths():
+def initpaths(platform):
     print "Initializing..."
     import os
+    import numpy as np
     p = []
-    p.append("D:\\fmri\\LanguageMVPA")
-    p.append("D:\GitHub\LanguageMVPA\multivariate\python")
-    p.append(os.path.join(p[0], "betas", "tstat"))
-    p.append(os.path.join(p[0], "masks", "sub"))
+    if 'win' in platform:
+        p.append("D:\\fmri\\LanguageMVPA")
+        p.append("D:\GitHub\LanguageMVPA\multivariate\python")
+    elif 'mac' in platform:
+        p.append("/Volumes/fmri/LanguageMVPA")
+        p.append('/Users/njchiang/GitHub/LanguageMVPA/multivariate/python')
+    elif 'usb' in platform:
+        p.append("/Volumes/JEFF/UCLA/LMVPA/")
+        p.append('/Users/njchiang/GitHub/LanguageMVPA/multivariate/python')
+
     p.append(os.path.join(p[1], "labels"))
     p.append(os.path.join(p[0], "Maps", "PyMVPA"))
-    c = ["stim", "verb", "anim", "AP", "CR", "syntax"]
+    # c = ["stim", "verb", "anim", "AP", "CR", "syntax"]
+
+    tmpc=np.loadtxt(os.path.join(p[1], 'labels', 'conds_key.txt'), dtype=str)
+    c={}
+    for i, name in enumerate(tmpc[0, :]):
+        c[name] = tmpc[1:,i]
+
     # s = ["LMVPA001", "LMVPA002", "LMVPA003", "LMVPA005", "LMVPA006", "LMVPA007", "LMVPA008", "LMVPA009", "LMVPA010",
     #        "LMVPA011", "LMVPA013", "LMVPA014", "LMVPA015", "LMVPA016", "LMVPA017", "LMVPA018", "LMVPA019"]
     s = {"LMVPA001": ["Run1", "Run2", "Run3", "Run4"],
@@ -36,15 +47,18 @@ def initpaths():
     m = ["left_IFG_operc", "left_IFG_triang", "left_STG_post", "left_MTG_post", "grayMatter"]
     return p, s, c, m
 
-
+######################################
+# load data
 def loadsubbetas(p, s, m="grayMatter", a=0):
     # load subject data with paths list, s: sub, c: contrast, m: mask
     print s
     import os
     bsn = str(s + "_LSA_Series.nii.gz")
-    bs = os.path.join(p[2], bsn)
+    bsp = os.path.join(p[0], "betas", "tstat")
+    bs = os.path.join(bsp, bsn)
+    mnp = os.path.join(p[0], s, "masks")
     mn = str(s+"_"+m+".nii.gz")
-    mf = os.path.join(p[3], mn)
+    mf = os.path.join(mnp, mn)
     from mvpa2.datasets.mri import fmri_dataset
     if a != 0:
         return fmri_dataset(samples=bs, targets=a.targets, chunks=a.chunks, mask=mf)
@@ -62,16 +76,17 @@ def loadrundata(p, s, r, m=None, c='trial_type'):
     from mvpa2.datasets.mri import fmri_dataset
     from mvpa2.datasets.sources import bids as bids
     efn = pjoin(p[0], 'data', s, 'func', s + '_' + r + '.tsv')
-    fe = bids.load_events(efn, 0)
+    fe = bids.load_events(efn)
     e = adjustevents(fe, c)
     # bfn = pjoin(p[0], 'data', s, 'func', 'extra', s+'_'+r+'_mc.nii.gz')
     # motion corrected and coregistered
-    bfn = pjoin(p[0], 'data', s, 'func', s+'_'+r+'.nii.gz')
+    bfn = pjoin(p[0], 'data', s, 'func', s + '_' + r + '.nii.gz')
     if m is not None:
         m = pjoin(p[0], 'data', s, 'masks', s+'_'+m+'.nii.gz')
         d = fmri_dataset(bfn, chunks=int(r.split('n')[1]), mask=m)
     else:
         d = fmri_dataset(bfn, chunks=int(r.split('n')[1]), mask=m)
+    # This line-- should be different if we're doing GLM, etc.
     return er.assign_conditionlabels(d, e, noinfolabel='rest')
 
 
@@ -83,6 +98,25 @@ def adjustevents(e, c='trial_type'):
             ee.append({'onset': np.round(d['onset']), 'duration': np.round(d['duration']), 'condition': d[c], 'intensity': 1})
         return ee
 
+
+def replacetargets(d, ckey, c='trial_type'):
+
+    import numpy as np
+    if c in ckey:
+        d.targets = [ckey[c][np.where(st == ckey['trial_type'])[0][0]] for st in d.targets]
+    else:
+        print "not a valid contrasts, did not do anything."
+    return d
+
+
+def loadevents(p, s, c='trial_type'):
+    fds = {}
+    from mvpa2.datasets.sources import bids
+    from os.path import join as pjoin
+    for sub in s.keys():
+        fds[sub] = [adjustevents(bids.load_events(pjoin(p[0], 'data', sub, 'func', sub + '_' + r + '.tsv')), c)
+            for r in s[sub]]
+    return fds
 
 def loadsubdata(p, s, m=None, c='trial_type'):
     from mvpa2.base import dataset
@@ -96,7 +130,19 @@ def loadsubdata(p, s, m=None, c='trial_type'):
 # a=0 indicates that the dataset attributes of the first run should be used
 # for the merged dataset
 
+def loadmotionparams(p, s):
+    import numpy as np
+    import os
+    res = {}
+    for sub in s.keys():
+        mcs = [np.loadtxt(os.path.join(p[0], 'data', sub, 'func', 'extra',
+                                       sub + '_' + r + '_mc', sub + '_' + r + '_mc.par'))
+               for r in s[sub]]
+        res[sub] = np.vstack(mcs)
+    return res
 
+######################################
+# classification
 def makebinarymodel(l):
     import numpy as np
     m = np.zeros([len(l), len(l)])
@@ -123,12 +169,10 @@ def initrsa(c_all, p):
     from mvpa2.measures import rsa
     tdsm = rsa.PDistTargetSimilarity(rsa.squareform(m))
     return ChainLearner([tdsm, TransposeMapper()])
-
-
 ######################################
 # quality control:
 
-def testsg(ds, w, p, voxIdx):
+def testsg(ds, w, p, voxIdx, c='chunks'):
     import matplotlib.pyplot as plt
     import numpy as np
     from scipy.signal import savgol_filter
@@ -138,18 +182,18 @@ def testsg(ds, w, p, voxIdx):
     poly1 = ds.copy(deep=False)
     poly2 = ds.copy(deep=False)
     t = np.arange(ds.shape[0])
-    poly_detrend(poly0, polyord=0, chunks_attr='chunks')
+    poly_detrend(poly0, polyord=0, chunks_attr=c)
     sgFilt = ds.copy(deep=False)
     manualSet = ds.copy(deep=False)
-    manual =  getSGDrift(manualSet, w, p)
+    manual = getSGDrift(manualSet, w, p)
     # filterMe = filterMat[:,voxIdx]
     # manual = savgol_filter(filterMe, w, p, axis=0)
     # AXIS SHOULD BE 0
     # manualMat = savgol_filter(filterMat, w, p, axis=0)
-    poly_detrend(poly1, polyord=1, chunks_attr='chunks')
-    poly_detrend(poly2, polyord=2, chunks_attr='chunks')
+    poly_detrend(poly1, polyord=1, chunks_attr=c)
+    poly_detrend(poly2, polyord=2, chunks_attr=c)
     # sgFilt = savgol_filter(sgFilt, w, p, axis=0)
-    sg.sg_filter(sgFilt, window_length=w, polyorder=p, chunks_attr='chunks', axis=0)
+    sg.sg_filter(sgFilt, window_length=w, polyorder=p, chunks_attr=c, axis=0)
     plt.plot(t, poly0.samples[:, voxIdx], 'k', label='demean')
     plt.plot(t, poly1.samples[:, voxIdx], 'b+', label='linear')
     plt.plot(t, poly2.samples[:, voxIdx], 'b*', label='quadratic')
@@ -168,7 +212,7 @@ def getSGDrift(d, w, p, chunks_attr='chunks'):
         reg = []
         for n, chunk in enumerate(uchunks):
             cinds = d.sa[chunks_attr].value == chunk
-            thisreg = sgfilter(ds[cinds].copy(deep=False), w, p) - np.mean(ds[cinds], axis=0)
+            thisreg = sgfilter(d[cinds].copy(deep=False), w, p) - np.mean(d[cinds], axis=0)
             reg.append(thisreg)
         reg = np.vstack(reg)
 
