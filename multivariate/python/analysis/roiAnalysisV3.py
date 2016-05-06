809303f92713668e3a6b465232f2f3ac8d95d9ad
Jeff <jeff.njchiang@gmail.com>
2016-05-05 23:31:08 -0700

    update ROI

--- multivariate/python/analysis/roiAnalysisV3.py
+++ multivariate/python/analysis/roiAnalysisV3.py
@@ -12,37 +12,83 @@ import numpy as np
 if sys.platform == 'darwin':
     plat = 'mac'
     sys.path.append('/Users/njchiang/GitHub/LanguageMVPA/multivariate/python/utils')
+    debug=True
 else:
     plat = 'win'
     sys.path.append('D:\\GitHub\\LanguageMVPA\\multivariate\\python\\utils')
-
+    debug=False
 import lmvpautils as lmvpa
+from string import Template
 # plat = 'usb'
 paths, subList, contrasts, maskList = lmvpa.initpaths(plat)
-thisContrast = 'syntax'
-roi = 'left_IFG_operc'
-filterLen = 49
-filterOrd = 3
-debug=True
+thisContrast = 'verb'
+chance = .125
+roi = 'grayMatter'
+nVox = 1000  # number of voxels to select
+nComp = 10  # number of SVD components
+filterLen = 49  #for SG filter
+filterOrd = 3  # for sg filter
 if debug:
     subList = {'LMVPA005': subList['LMVPA005']}
-# load things in as trial type for easy regression, then swap out labels accordingly
-# do we actually want to load all data simultaneously? or one brain at a time...
+# this would load all data at once. not memory efficient
 # ds_all = lmvpa.loadsubdata(paths, subList, m=roi, c='trial_type')
 # motion parameters for all subjects
 mc_params = lmvpa.loadmotionparams(paths, subList)
 # events for beta extraction
-# beta_events = lmvpa.loadevents(paths, subList, c='trial_type')
 # add everything as a sample attribute
 beta_events = lmvpa.loadevents(paths, subList)
 
-import copy
-import os
 from mvpa2.datasets.mri import map2nifti
 from mvpa2.mappers.zscore import zscore
 import SavGolFilter as sg
 import mvpa2.datasets.eventrelated as er
+from mvpa2.mappers.svd import SVDMapper
+from mvpa2.datasets.base import StaticFeatureSelection, ChainMapper
+from mvpa2.featsel.helpers import FixedNElementTailSelector
+import mvpa2.featsel as fs
+from mvpa2.clfs import svm
+from mvpa2.clfs.warehouse import OneWayAnova, LDA
+from mvpa2.clfs.meta import FeatureSelectionClassifier, MappedClassifier
+from mvpa2.measures.base import CrossValidation
+from mvpa2.generators.partition import NFoldPartitioner
+from mvpa2.misc import errorfx
+from mvpa2.clfs import gda
+# import mvpa2.misc.plot as pt
+#initialize classifier
+# clf = lm.BayesianRidge()
+# clf.fit(desX.matrix, thisDS.samples[:,1])
+# clf.predict(desX.matrix)
+# make class that takes skl regression models as input and wraps into mapper
+cname = 'LinearSVM'
+clf = svm.LinearCSVMC()
+preproc = 'None'
+# feature selection
+# preproc = 'fsel-'+str(nVox)
+# fsel=LDA()
+fsel = OneWayAnova()
+fselector = fs.helpers.FixedNElementTailSelector(nVox, tail='upper',
+                                                 mode='select', sort=False)
+# fselector = fs.helpers.FractionTailSelector(0.05, mode='select', tail='upper')
+sbfs = fs.base.SensitivityBasedFeatureSelection(fsel, fselector,
+                                                enable_ca=['sensitivities'])
+fclf = FeatureSelectionClassifier(clf, sbfs)
 
+# SVD
+# preproc = 'SVD-' + str(nComp)
+# svdmapper = SVDMapper()
+# get_SVD_sliced = lambda x: ChainMapper([svdmapper, StaticFeatureSelection(x)])
+# fclf = MappedClassifier(clf, get_SVD_sliced(slice(0, nComp)))
+
+################################################
+cv = CrossValidation(fclf,
+                     NFoldPartitioner(attr='chunks'),
+                     errorfx=errorfx.mean_match_accuracy)
+
+lresults = []
+presults = []
+l2presults=[]
+p2lresults=[]
+labels = []
 for sub in subList.keys():
     thisSub = {sub: subList[sub]}
     dsdict = lmvpa.loadsubdata(paths, thisSub, m=roi, c='trial_type')
@@ -63,11 +109,9 @@ for sub in subList.keys():
     # refit events and regress...
     # get timing data from timing files
     rds, events = lmvpa.amendtimings(thisDS.copy(), beta_events[sub])
-    # rds.sa['targets'] = rds.sa[thisContrast].values
-    desX, rds = lmvpa.make_designmat(rds, events, time_attr='time_coords', condition_attr=['trial_type'],
-                                design_kwargs={'add_regs': mc_params[sub], 'hrf_model': 'canonical'},
-                                glmfit_kwargs=None, regr_attrs=None)
+
     #OLS beta extraction
+    betas = 'LSA'
     evds = er.fit_event_hrf_model(rds, events, time_attr='time_coords',
                                   condition_attr=('trial_type',  'chunks'),
                                   design_kwargs={'add_regs': mc_params[sub], 'hrf_model': 'canonical'},
@@ -75,33 +119,96 @@ for sub in subList.keys():
 
     fds = lmvpa.replacetargets(evds, contrasts, thisContrast)
     fds = fds[fds.targets != '0']
+    fds = fds[fds.targets != 'rest']
     lidx = fds.chunks < fds.sa['chunks'].unique[len(fds.sa['chunks'].unique)/2]
     pidx = fds.chunks >= fds.sa['chunks'].unique[len(fds.sa['chunks'].unique) / 2]
 
     lfds = fds[lidx]
     pfds = fds[pidx]
-    # split data into language and pictures
 
-
-    from sklearn import linear_model as lm
-    from mvpa2.clfs.skl.base import SKLLearnerAdapter
-    clf = SKLLearnerAdapter(lm.BayesianRidge())
-
-    from mvpa2.clfs import svm
-    clf = svm.LinearCSVMC()
-    from mvpa2.measures.base import CrossValidation
-    from mvpa2.generators.partition import NFoldPartitioner
-    from mvpa2.misc import errorfx
-    cv = CrossValidation(clf,
-                         NFoldPartitioner(attr='chunks'),
-                         errorfx=errorfx.mean_match_accuracy)
     lres = cv(lfds)
+    lresults.append(lres)
     print "language: " + str(np.mean(lres.samples))
     cv.untrain()
     pres = cv(pfds)
+    presults.append(pres)
     print "pictures: " + str(np.mean(pres.samples))
+    labels.append(sub)
+    fclf.train(fds[lidx])
+    l2presults.append(np.mean(fclf.predict(fds[pidx]) == fds[pidx].sa.targets))
+    fclf.untrain()
+    fclf.train(fds[pidx])
+    p2lresults.append(np.mean(fclf.predict(fds[lidx]) == fds[lidx].sa.targets))
 
-# clf = lm.BayesianRidge()
-# clf.fit(desX.matrix, thisDS.samples[:,1])
-# clf.predict(desX.matrix)
-# make class that takes skl regression models as input and wraps into mapper
+
+def plotsubs(lr, pr, l2p, p2l, c=None, title=None, bar_width=.2, opacity=.4, error_config={'ecolor': '0.3'}):
+    import matplotlib.pyplot as plt
+    # results is the concatenated output of cv across subjects... or something.
+    f, (ax1, ax2) = plt.subplots(2, figsize=(12,6))
+    index = np.arange(len(lr))
+    lheights = []
+    lerrbars = []
+    pheights = []
+    perrbars = []
+
+    for i in lr:
+        lheights.append(np.mean(i.samples))
+        lerrbars.append(np.std(i.samples))
+    lheights = np.array(lheights)
+    lstd = np.array(lerrbars)
+
+    rects1 = ax1.bar(index, lheights, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     yerr=lstd,
+                     error_kw=error_config,
+                     label='Language')
+    for i in pr:
+        pheights.append(np.mean(i.samples))
+        perrbars.append(np.std(i.samples))
+    pheights=np.array(pheights)
+    pstd = np.array(perrbars)
+    rects2 = ax1.bar(index+bar_width, pheights, bar_width,
+                     alpha=opacity,
+                     color='r',
+                     yerr=pstd,
+                     error_kw=error_config,
+                     label='Picture')
+
+
+    l2pheights=np.array(l2p)
+    rects3 = ax1.bar(index+bar_width+bar_width, l2pheights, bar_width,
+                     alpha=opacity,
+                     color='g',
+                     error_kw=error_config,
+                     label='L2P')
+
+    p2lheights=np.array(p2l)
+    rects4 = ax1.bar(index+bar_width+bar_width+bar_width, p2lheights, bar_width,
+                     alpha=opacity,
+                     color='y',
+                     error_kw=error_config,
+                     label='P2L')
+
+    if not c is None:
+        ax1.plot((0, len(lr)), (c,c), 'k-', alpha=opacity/2, label='chance')
+        ax2.plot((0, len(lr)), (c,c), 'k-', alpha=opacity/2, label='chance')
+    ax1.set_xlabel('Subjects')
+    ax1.set_ylabel('Accuracy')
+    ax2.set_ylabel('Accuracy')
+    if not desc is None:
+        ax1.set_title(title)
+    else:
+        ax1.set_title('Classification Accuracies')
+
+    ax1.legend()
+    # ax2.legend()
+    ax1.set_ylim(0, 1)
+    ax2.set_ylim(0, 1)
+    ax2.boxplot([lheights, pheights, l2pheights, p2lheights], labels=['Language', 'Pictures', 'L2P', 'P2L'])
+    plt.show()
+
+
+desc = Template('$con: SG filter($fl, $fo) Classifier($c) Preproc($p) Mask($m) Betas($b)')
+title = desc.substitute(con=thisContrast, fl=str(filterLen), fo=str(filterOrd), c=cname, p=preproc, m=roi, b=betas)
+plotsubs(lresults, presults, l2presults, p2lresults, c=chance, title=title)
