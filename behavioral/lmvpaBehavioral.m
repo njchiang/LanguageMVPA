3c2c7d145b6224bf689248e4a3e2ee6df4a8837a
njchiang <jeff.njchiang@gmail.com>
2016-06-15 10:59:32 -0700

    update behavioral

--- behavioral/lmvpaBehavioral.m
+++ behavioral/lmvpaBehavioral.m
@@ -2,7 +2,8 @@
 
 %% parameters
 DEBUG=true;
-screenRatio = [.1 .1 .8 .85]; %figure this out
+textSize=72;
+screenRatio = [0 0 .5 .5]; % offsetX offsetY sizeX sizeY
 %the total number of sentences
 nTrialTypes = 4;
 %set the indices
@@ -12,7 +13,7 @@ nBlocks=4;
 %the random time between showing sentences
 displaytime=1:4;
 timeout = 2;
-
+barSize = [100 20];
 
 %% Part One: Initialization
 Screen('Preference', 'VisualDebugLevel', 1);
@@ -43,22 +44,22 @@ shuffledPairs = randPairs(pairIdx1, pairIdx2);
 input('Press <enter> to begin');
 outfilename = ['lmvpa_' sinit];
 % [win,Scrnsize] = Screen('OpenWindow',0);
-sizeScreen=get(0,'ScreenSize'); 
-sizeScreen=[sizeScreen(3:end),sizeScreen(3:end)];
-[win, Scrnsize]=Screen('OpenWindow',1,...
-    [255 255 255],sizeScreen.*screenRatio);
+fullScreenSize=get(0,'ScreenSize');
+[win, ScreenSize]=Screen('OpenWindow',0,...
+    [255 255 255],fullScreenSize.*screenRatio);
 halfFlip = Screen('GetFlipInterval', win)/2;
 KbName('UnifyKeyNames');
-centerX = Scrnsize(3)/2;
-centerY = Scrnsize(4)/2;
+centerX = ScreenSize(3)/2;
+centerY = ScreenSize(4)/2;
 Screen('TextFont',win, 'Arial');
-Screen('TextSize',win, 72);
+Screen('TextSize',win, textSize);
 
 %get the size of the fixation cross
 [bounds] = Screen('TextBounds', win, '+');
 fixSizeX = bounds(3)/2;
 
-%get the size of the stimuli
+%%%%%%%%%%%%%%%%%%%%%%%
+%get the size of the stimuli, must be run per sentence
 [bounds] = Screen('TextBounds', win, 'S2');
 stimSizeX = bounds(3)/2;
 leftStimX = centerX-200-stimSizeX;
@@ -71,55 +72,49 @@ if (~DEBUG), HideCursor; end
 for blocknumber = 1:nBlocks
     blockLength = size(shuffledPairs,2)/nBlocks;
     theseTrials = shuffledPairs(:, ...
-        blockLength*(blocknumber-1) + 1:blockLength); % the trials for this block
+        blockLength*(blocknumber-1) + [1:blockLength]); % the trials for this block
     for trialnumber = 1:size(theseTrials, 2)
         displaytime=Shuffle(displaytime);
-         % Type of results L/R x on left right 
+        % Type of results L/R x on left right
         WaitSecs(2);   %pause at start of trial, then show fixation
-
+        
         %%%%%%%%%%% fixSizeX should be sentence 1 offset %%%%%%%%%%%%%%
         Screen('DrawText', win , Sentence{shuffledPairs(1, trialnumber)},...
             centerX-fixSizeX , centerY,[0,0,0]);
         onsetTime1 = Screen('Flip',win);
-
+        
         %Draw the fixation cross
         Screen('DrawText', win , '+',centerX-fixSizeX ,...
-               centerY,[0,0,0]);
-
+            centerY,[0,0,0]);
+        
         %a flip and show sentence two
         onsetTime2 = Screen('Flip',win,onsetTime1 + displaytime(1)/2 ...
-                - halfFlip);
+            - halfFlip);
         %%%%%%%%%%% fixSizeX should be sentence 2 offset %%%%%%%%%%%%%%%
         Screen('DrawText', win , Sentence{shuffledPairs(2, trialnumber)}, ...
             centerX-fixSizeX, centerY,[0,0,0]);
         %a flip and show the cross
         onsetTime3 = Screen('Flip',win,onsetTime2 + displaytime(2)/2 ...
-                - halfFlip);
+            - halfFlip);
         Screen('DrawText', win , '+',centerX-fixSizeX ,...
-                centerY,[0,0,0]);
+            centerY,[0,0,0]);
         %a flip and the next round
         onsetTime4 = Screen('Flip',win,onsetTime3 + displaytime(3)/2 ...
-                - halfFlip);
-        Screen('DrawText', win , 'Question goes here', ...
-            centerX-fixSizeX, centerY,[0,0,0]);
-        Starttime  = Screen('Flip',win,onsetTime4 + displaytime(4)/2 ...
-                - halfFlip);
-        Nowtime = Starttime;
+            - halfFlip);
+
         % collect rating here:
-        % [rating, respTime] = slidingBar(window); % this won't work
-        rating = rand;
-        respTime = rand;
+        [rating, respTime, ratinginit] = slidingBar(win, barSize, ScreenSize);
+        %         respTime = rand;
         results(shuffledPairs(1, trialnumber), shuffledPairs(2, trialnumber), 1) = rating;
         results(shuffledPairs(1, trialnumber), shuffledPairs(2, trialnumber), 2) = respTime;
-
+        
     end
- Screen('DrawText', win , 'End of block... press Enter to continue', ...
-            centerX-fixSizeX, centerY,[0,0,0]);
- Starttime  = Screen('Flip',win,onsetTime4 + displaytime(4)/2 ...
-        - halfFlip); 
-% get some sort of button press here
-input('Press <enter>');
-
+    Screen('DrawText', win , 'End of block... press Enter to continue', ...
+        centerX-fixSizeX, centerY,[0,0,0]);
+    Screen('Flip', win);
+    % get some sort of button press here
+    input('Press <enter>');
+    
 end
 %% Part Three: Cleanup and File save
 ShowCursor
--- behavioral/slidingBar.m
+++ behavioral/slidingBar.m
@@ -1,45 +1,40 @@
-function [resp] = slidingBar(window, squaresize)
+function [rating, nextTime, ratinginit] = slidingBar(window, squaresize, ScreenSize)
 % this function draws a sliding bar on to PTB Screen window of length
 % squaresize
-
-squaresize = 100;
-
-ratenum = [-1 1 ];  %no, yes
+textSize = 40;
 rating = round(rand(1,1)*100)/100; % start at random position
 ratinginit = rating; % save initial position
-ypos = centeri(i,2)+framesize/2+100;
-	squaresize = 100;
-    xstart = centeri(i,1);%+ratenum(ratei)*(squaresize*1.5);
-    squarerange=[xstart-squaresize ypos-squaresize/5 xstart+squaresize ypos+squaresize/5];
-    Screen('FrameRect',window,[200 200 200], squarerange,[3]);
-    squarerangersp=[xstart-squaresize ypos-squaresize/5 xstart-squaresize+2*squaresize*rating ypos+squaresize/5];
-    Screen('FillRect',window,[200 200 200], squarerangersp);
-    
-    Screen('TextSize',window,40);
-    Screen('DrawText',window,'NO(0%)',xstart-squaresize-180, ypos+squaresize/5,[200 200 200]);%
-    Screen('DrawText',window,'YES(100%)',xstart+squaresize+30, ypos+squaresize/5,[200 200 200]);%
+ypos = ScreenSize(4)/2; % random starting point
+xstart = ScreenSize(3)/2; % random starting point
+squarerange=[xstart-squaresize(1) ypos-squaresize(2) xstart+squaresize(1) ypos+squaresize(2)];
+Screen('FrameRect',window,[200 200 200], squarerange,[3]);
+squarerangersp=[xstart-squaresize(1) ypos-squaresize(2) xstart-squaresize(1)+2*squaresize(1)*rating ypos+squaresize(2)];
+Screen('FillRect',window,[200 200 200], squarerangersp);
 
-    ShowCursor(0);
-    
-    FlushEvents('keyDown');
-    
-    % Wait for a click and hide the cursor
-	Screen('TextSize',window,30);
-	Screen('DrawText',window,'Drag mouse (i.e. hold button down) to response',xstart-400,ypos-500,[200 200 200]);
-	Screen('DrawText',window,[sprintf('%d',round(rating*100)) '%'],xstart-30, ypos+80,[200 200 200]);%
-        Screen('Flip',window);
+Screen('TextSize',window, textSize);
+% Screen('DrawText',window,'NO(0%)',xstart-squaresize(1)-180, ypos+squaresize(2),[200 200 200]);%
+% Screen('DrawText',window,'YES(100%)',xstart+squaresize(1)+30, ypos+squaresize(2),[200 200 200]);%
 
-    while (1)
-		[x,y,buttons] = GetMouse;
-		if buttons(1)
-		  break;
-		end
+ShowCursor(0);
+
+FlushEvents('keyDown');
+
+% Wait for a click and hide the cursor
+% Screen('DrawText',window,'Drag mouse (i.e. hold button down) to respond',xstart-400,ypos-500,[200 200 200]);
+Screen('DrawText',window,[sprintf('%d',round(rating*100)) '%'],xstart-30, ypos+80,[200 200 200]);%
+Screen('Flip',window);
+
+while (1)
+    [x,y,buttons] = GetMouse;
+    if buttons(1)
+        break;
     end
-    
+end
+
 % Loop and track the mouse, drawing the contour
 [theX,theY] = GetMouse;
 thePoints = [theX theY];
-squarerangersp=[xstart-squaresize ypos-squaresize/5 theX ypos+squaresize/5];
+squarerangersp=[xstart-squaresize(1) ypos-squaresize(2) theX ypos+squaresize(2)];
 Screen('FillRect',window,[200 200 200], squarerangersp);
 
 % 	Screen(theWindow,'DrawLine',255,theX,theY,theX,theY);
@@ -47,26 +42,26 @@ sampleTime = 0.01;
 startTime = GetSecs;
 nextTime = startTime+sampleTime;
 while (1)
-    [x,y,buttons] = GetMouse;	
+    [x,y,buttons] = GetMouse;
     if ~buttons(1)
         break;
     end
-    if (x ~= theX | y ~= theY) & x>=xstart-squaresize & x<=xstart+squaresize
-
-        squarerangersp=[xstart-squaresize ypos-squaresize/5 theX ypos+squaresize/5];
+    if (x ~= theX || y ~= theY) && x>=xstart-squaresize(1) && x<=xstart+squaresize(1)
+        
+        squarerangersp=[xstart-squaresize(1) ypos-squaresize(2) theX ypos+squaresize(2)];
         Screen('FillRect',window,[0 0 0], squarerange);
         Screen('DrawText',window,[sprintf('%d',round(rating*100)) '%'],xstart-30, ypos+80,[0 0 0]);%
         Screen('FrameRect',window,[200 200 200], squarerange,[3]);
         Screen('FillRect',window,[200 200 200], squarerangersp);
         theX = x; theY = y;
-
-        rating = (theX-(xstart-squaresize))/2/squaresize;
+        
+        rating = (theX-(xstart-squaresize(1)))/2/squaresize(1);
         Screen('DrawText',window,[sprintf('%d',round(rating*100)) '%'],xstart-30, ypos+80,[200 200 200]);%
-
+        
         %%%%%%%%%%%%%%%%%%%
-            Screen('Flip',window); % deletes the rest of the screen
+        Screen('Flip',window); % deletes the rest of the screen
         %%%%%%%%%%%%%%%%%%%%
-
+        
     end
     if (GetSecs > nextTime)
         thePoints = [thePoints ; x y];
