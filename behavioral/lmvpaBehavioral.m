83d7a1ec588578c96a701154ef52342f1eb1f907
MontiLabUCLA <montilab@yahoo.com>
2016-06-28 14:06:46 -0700

    edit behavioral

--- behavioral/lmvpaBehavioral.m
+++ behavioral/lmvpaBehavioral.m
@@ -1,20 +1,24 @@
 function [] = lmvpaBehavioral()
 % PTB Script that runs a full experiment testing the LMVPA
 % to do... only probe upper or lower triangle
+% maybe we want to incrementally save after each block...
 %% parameters to adjust
 DEBUG_MODE=false;
 
 textSize=32;
 screenRatio = [0 0 .5 .5]; % offsetX offsetY sizeX sizeY
 % the total number of sentences
-nTrialTypes = 32;
+nTrialTypes = 4;
+nBlocks=1;
+
 % set the indices of sentences to be used
-trialIdx=[1:nTrialTypes];
-nBlocks=8;
 displayTimeMean=2; % randomly sample from exponential with expectation 2s
 fixDurMean = 1; % randomly sample from exponential with expectation 1s
 barSize = [100 20];
 trialSelector = 'upper'; % 'upper', 'lower', or 'all'
+
+% probably make other matrices for syntax and semantics
+load('sentences.mat');
 
 %% Part One: Initialization
 if strcmpi(DEBUG_MODE, 'true')
@@ -31,9 +35,9 @@ catch
     fprintf(['Invalid seed, using ' num2str(seed)]);
 end
 
-load('sentences.mat');
 results = zeros([nTrialTypes, nTrialTypes, 2]);
 rng(seed)
+trialIdx=[1:nTrialTypes];
 shuffledPairs = shuffleIdx(trialIdx, nTrialTypes);
 % top half or bottom half of matrix depending on sign
 if strcmp('upper', trialSelector)
@@ -46,7 +50,12 @@ end
 shuffledPairs = shuffledPairs(:, triangleSelector);
 
 fixDurVec =  exprnd(fixDurMean, size(shuffledPairs));
+fixDurVec(fixDurVec < 1.5) = 1.5;
+fixDurVec(fixDurVec > 2.5) = 2.5;
 displayTimeVec = exprnd(displayTimeMean, size(shuffledPairs));
+displayTimeVec(displayTimeVec < .5) = .5;
+displayTimeVec(displayTimeVec > 1.5) = 1.5;
+
 
 input('Press <enter> to begin');
 fullScreenSize=get(0,'ScreenSize');
@@ -76,36 +85,38 @@ for blocknumber = 1:nBlocks
         trialStartTime = Screen('Flip', win);
         
         %%%%%%%%%%% each trial %%%%%%%%%%%%%%
+        % display first sentence
         trial1Time = displayText(win, ScreenSize, ...
             Sentence{shuffledPairs(1, trialnumber)}, ...
             trialStartTime, theseFixTimes(1, trialnumber));
-        
+        % first fixation
         fix1Time = displayText(win, ScreenSize, '+', ...
             trial1Time, theseDisplayTimes(1, trialnumber));
-        
+        % second sentence
         trial2Time = displayText(win, ScreenSize, ...
             Sentence{shuffledPairs(2, trialnumber)}, ...
             fix1Time, theseFixTimes(2, trialnumber));
-        
+        % second fixation
         fix2Time = displayText(win, ScreenSize, '+', ...
             trial2Time, theseDisplayTimes(2, trialnumber));
-        
         % collect rating here:
         nowTime=GetSecs;
         [rating, ~, ratinginit] = slidingBar(win, barSize, ScreenSize);
-        
         respTime = GetSecs-nowTime;
         % reset text parameters
         Screen('TextFont',win, 'Arial');
         Screen('TextSize',win, textSize);
-        
+        % save results
         results(shuffledPairs(1, trialnumber), shuffledPairs(2, trialnumber), 1) = rating;
         results(shuffledPairs(1, trialnumber), shuffledPairs(2, trialnumber), 2) = respTime;
         
     end
     endBlockText='End of block... press Enter to continue';
     displayText(win, ScreenSize, endBlockText);
+    % incremental save
+    save(outfilename, 'results', 'shuffledPairs', 'seed', 'sinit');
     % press a button to advance
+    % probably should use Kb...
     input('Press <enter>');
     
 end
@@ -115,6 +126,7 @@ if (~DEBUG_MODE), ListenChar(0); end
 save(outfilename, 'results', 'shuffledPairs', 'seed', 'sinit');
 endExperimText='Thank you for participating. Press <enter> to exit';
 displayText(win, ScreenSize, endExperimText);
+% use kb here
 input('Press <enter>');
 sca;
 
